edbc47e1450e9a07c9e6acd0cae24c1e6e8eb731
kaungkhantthar7777 <kaungkhantthar@ucsm.edu.mm>
2020-07-21 19:25:05 +0630

    Fixed Redux store

--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,10 @@ export const configureStore = () => {
     rootReducer,
     compose(
       applyMiddleware(thunk),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
+      window.__REDUX_DEVTOOLS_EXTENSION__
+        ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        : (f) => f
     )
   );
   return store;
